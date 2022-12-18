0204b188ece03d656ef3e9dc8a6157ef5061d28b
keres <keres.kgb@gmail.com>
2022-12-17 19:59:26 -0500

    basic layout structure

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,19 @@
 <script setup>
 import Layout from './Layout.vue'
 import Header from './Header.vue'
+import Resume from './Resume/Index.vue'
+import Movements from './Movements.vue'
 </script>
 <template>
   <Layout>
     <template #header>
       <Header />
+    </template>
+    <template #resume>
+      <Resume />
+    </template>
+    <template #movements>
+      <Movements />
     </template>
   </Layout>
 </template>
